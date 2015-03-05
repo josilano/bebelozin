dbdf01d91d9b97bca3acdca46ab467f28029b986
Alcivan Silva <alcivansilva7@outlook.com>
2015-03-05 15:56:18 -0300

    Revert "Update and rename src/java/edu/br/bebelozin/Factory/ConnectionFactory.java to src/java/edu/br/bebelozin/Fábrica/ConnectionFactory.java"
    This reverts commit 56b2c7c1030e2a015566e367e9226f21ffc82024.

--- src/java/edu/br/bebelozin/Factory/ConnectionFactory.java
+++ src/java/edu/br/bebelozin/Factory/ConnectionFactory.java
@@ -18,7 +18,8 @@ import java.sql.SQLException;
 public class ConnectionFactory {
  
     private String driver = "org.postgresql.Driver";
-    private String url = "jdbc:postgresql://localhost/bebelozin", usuario = "postgres", senha = "";
+    private String url = "jdbc:postgresql://localhost/bebelozin", usuario = "postgres", senha = "94415944";
+    
     public Connection getConnection(){
         try{
             Class.forName(driver);
